fe02375e192f3e797a607a3c58534f31a1fbb844
Mijavitz <mijail2108@gmail.com>
2019-03-21 21:48:21 -0500

    Initial commit

---
+++ RegisterDocsForm/RegisterDocsForm/RegisterDocsForm/RegisterDocuments.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RegisterDocsForm
+{
+    public partial class RegisterDocuments : ContentPage
+    {
+        public RegisterDocuments()
+        {
+            InitializeComponent();
+            //btnSecondPage.Clicked += (sender, e) =>
+            //{
+            //    Navigation.PushAsync(new SecondPage()); 
+            //};
+        }
+
+        private void btnDriverLicense_Clicked(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new SecondContentPage());
+            Navigation.PushAsync(new DriverLicenseDoc());
+            //Navigation.PushModalAsync(new SecondPage()); //On top of this, all pages also supports PushModalAsync() which just push a new page on top of the existing one.
+
+        }
+
+        private void btnSoat_Clicked(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new SecondContentPage());
+            Navigation.PushAsync(new SoatDoc());
+            //Navigation.PushModalAsync(new SecondPage()); //On top of this, all pages also supports PushModalAsync() which just push a new page on top of the existing one.
+
+        }
+    }
+}
---
+++ RegisterDocsForm/RegisterDocsForm/RegisterDocsForm/SoatDoc.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegisterDocsForm
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SoatDoc : ContentPage
+	{
+		public SoatDoc()
+		{
+			InitializeComponent ();
+		}
+	}
+}
